f4e9312aeb28e377af8071370c00d52b6a8df496
Akhileshwar <gurramakhileshwar333@gmail.com>
2022-06-29 23:44:30 +0530

    Add about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { aboutme } from '../constants/aboutme'
+import { aboutme1, aboutme2, aboutme3 } from '../constants/aboutme'
 import Paragraph from '../utils/Paragraph';
 import Heading from '../utils/Heading'
 
@@ -7,12 +7,13 @@ import Heading from '../utils/Heading'
 // add avatar 
 const About = () => {
   return (
-    <div className='section section-dark'>
+    <div className='section '>
       <div className='section-content' id="aboutme">
         <Heading heading={"About me"}/>
         <Paragraph info={"Let's break the ice a little"}/>
-        <Paragraph info={aboutme}/>
-        <Paragraph info={aboutme}/>
+        <Paragraph info={aboutme1}/>
+        <Paragraph info={aboutme2}/>
+        <Paragraph info={aboutme3}/>
     </div>
     </div>
   )
---
+++ src/constants/skilldata.js
@@ -0,0 +1,45 @@
+const skills = [
+  {
+    id: 1,
+    icon: "fa-solid fa-code fa-4x icons",
+    name: "Web Development",
+    content: "Fast, responsive and engaging apps that bring your ideas to life."
+  },
+  {
+    id: 2,
+    icon: "fa-solid fa-mobile-alt fa-4x icons",
+    name: "Mobile Application Development",
+    content:
+      "Cross-platform apps built with efficiency and speed for Android and iOS."
+  },
+  {
+    id: 3,
+    icon: "fa-solid fa-cogs fa-4x icons",
+    name: "API Development",
+    content:
+      "REST APIs that are tailored to your needs and follow the best practices in performance and security"
+  },
+  {
+    id: 4,
+    icon: "fa-solid fa-link fa-4x icons",
+    name: "Third-party API Integration",
+    content:
+      "Integration with any third-party API of your choice. Extend the functionality of your apps with the least amount of effort."
+  },
+  {
+    id: 5,
+    icon: "fa-solid fa-database fa-4x icons",
+    name: "Database Design",
+    content:
+      "Proper Database design for effective Web & Mobile development, always aiming for performance, scale and stability."
+  },
+  {
+    id: 6,
+    icon: "fa-solid fa-cloud fa-4x icons",
+    name: "Cloud Integration",
+    content:
+      "Deployment and maintenance of your apps in a wide range of Cloud Services, from fully managed to highly customizable VPS."
+  }
+];
+
+export default skills;
