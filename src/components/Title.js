f67fd3f45e649cff09028feb9491ac1ce8fc467b
Maveric-k07 <gurramakhileshwar333@gmail.com>
2022-07-03 23:40:29 +0530

    update projectcard and resolve nav bugs

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,22 +4,19 @@ import Icons from "./Icons";
 // add button and background image
 
 const myStyle = {
-  backgroundImage: `url(${BackgroundImage})`,
-  backgroundPosition: "center",
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-  textAlign: "center"
+  paddingTop : "6%",
+  textAlight: "center"
 };
 
 const Title = () => {
   return (
     // <div id="title" style={myStyle}>
     <div className="container-fluid">
-      <h1>Full-stack</h1>
+      <h1 style={myStyle}>Full-stack</h1>
       <h1>
         <b>Web</b> and <b>Mobile</b>
       </h1>
-      <h1>developer</h1>
+      <h1>Developer</h1>
       <br />
       <Icons />
       <a href="#skills" class="btn btn-outline-light btn-lg title-button">
