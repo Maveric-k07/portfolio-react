b3e1baabebad7d3a437b574f421e650aa0f82e7c
keerthanapalepu <keerthanapalepu2002@gmail.com>
2022-07-11 21:30:46 +0530

    update tech card and styles

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -34,30 +34,30 @@ const Tech = () => {
           <div className="row" >
             <div className="TechCard p-3 mb-2">
             <p className="tech">FRONTEND </p>
-            {frontend.map(TechCardData)}
             <img className="techStackImage" src={frontendImg} alt="frontend" />
+            {frontend.map(TechCardData)}
           </div>
           </div>
 
-
+          
           <div className="row">
             <div className="TechCard p-3 mb-2">
             <p className="tech">BACKEND </p>
-            <img className="techStackImage" src={backendImg} alt="backend" />
             {backend.map(TechCardData)}
+            <img className="techStackImage" src={backendImg} alt="backend" />
           </div>
-
+          </div>
 
           <div className="row" >
             <div className="TechCard p-3 mb-2">
             <p className="tech">DATABASES </p>
-              <img className="techStackImage" src={databaseImg} alt="database" />
-              {databases.map(TechCardData)}
+            <img className="techStackImage" src={databaseImg} alt="database" />
+            {databases.map(TechCardData)}
           </div>
           </div>
 
 
-          </div>
+
           <div className="row" >
             <div className="TechCard p-3 mb-2">
             <p className="tech">LANGUAGES </p>
